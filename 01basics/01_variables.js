e6f0cb4cd8af3cf7a3f02973c7668a019f362342
$Naresh kumar <121456681+mr-naresh-kumar@users.noreply.github.com>
2025-05-15 06:19:29 +0000

    container config done

---
+++ 01basics/01_variables.js
@@ -0,0 +1,30 @@
+const accountId = 1444552
+
+let accountemail = "nareshbaghel274@gmail.com"
+
+var accountPassword = "12344"
+var abs = null;
+
+let accountState = null;
+
+accountCity = "Jaipur"
+
+
+// accountId =2  -> us not allowed
+
+accountemail = "naresh@gmail"
+accountPassword ="2121323"
+accountCity = "Bengaluru"
+
+
+console.log(accountId);
+
+// prefer not to use var 
+// becauses of issuse in block scope and functional scope 
+
+
+console.table([accountId,accountPassword,accountemail,accountCity, accountState, abs])
+
+
+
+
