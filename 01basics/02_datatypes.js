80bc074621fbf85123cf9aceb64f76e1c0781d0f
$Naresh kumar <121456681+mr-naresh-kumar@users.noreply.github.com>
2025-05-15 06:53:32 +0000

    datatypes

---
+++ 01basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use struct"; // treat all js code as newer version 
+
+// alert("3+3") // we are useing node.js, not browser
+
+
+let name  = "naresh"
+let age = 18
+let isLoogedIN  = false
+
+
+//  number  2to power 53 
+// bgiInt 
+
+// string => ""
+// boolean= > true/false
+// null  =>standlone value
+// undefined =>  value is not asign 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof "naresh"); // output is string 
+
+console.log(typeof null); // output is object 
+console.log(typeof  undefined)  // output is undefined
+
+
+
+
+
