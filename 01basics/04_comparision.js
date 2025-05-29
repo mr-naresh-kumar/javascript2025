13b117df982954499ac1097e80eb8ecee1c149a2
$Naresh kumar <121456681+mr-naresh-kumar@users.noreply.github.com>
2025-05-29 04:24:49 +0000

    comparison

---
+++ 01basics/04_comparision.js
@@ -0,0 +1,14 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+
+// console.log("2" > 1);
+
+console.log(null == undefined); // true
+console.log(null === undefined); // false
+console.log(null > 0); // false
+console.log(null < 0); // false
+console.log(null >= 0); // true
+console.log(null == 0); // false
+console.log(0==null); // false
