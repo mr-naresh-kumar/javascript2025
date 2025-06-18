aefe48dab1fee68e99b3a2923cf59c41be9c4d91
$Naresh kumar <121456681+mr-naresh-kumar@users.noreply.github.com>
2025-06-18 05:30:36 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,50 @@
+//if
+
+// if(condition) {
+//     // code to execute if condition is true
+// }
+
+
+// const isUserLoggedIn = true;
+
+// if(isUserLoggedIn) {
+//     console.log("User is logged in");
+// }
+
+// const temperature = 30;
+
+// if (temperature > 25) {
+//     console.log("It's a hot day");
+// }
+// console.log("This will always execute");
+// <,>, <=, >=, ==, !=, ===, !== 
+
+
+// const score  = 200
+// if(score>100){
+//     const power = "fly";
+//     console.log(`User has power to: ${power}`);
+
+// }
+//  console.log(`User has power to ${power}`);
+
+const balance = 1000;
+
+if(balance > 500) console.log("You can withdraw money")
+
+
+    const isUserLoggedIn = true;
+    const debitCard = true;
+    const loggedFromGoogle = false;
+    const loggedInFromEmail = true;
+
+    if(loggedFromGoogle || loggedInFromEmail){
+        console.log("You are logged in from Google or Email");
+    }
+
+    if(isUserLoggedIn && debitCard) {
+        console.log("You can withdraw money using debit card");
+    }
+
+
+    
