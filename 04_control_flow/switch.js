2ef0e67943c09d7224039c2aceeb558fcb7b04b6
$Naresh kumar <121456681+mr-naresh-kumar@users.noreply.github.com>
2025-06-18 05:39:52 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,27 @@
+// switch 
+// switch(expression){
+//     case value:
+//         break; // optional
+
+//     default: // optional
+//     break;
+// }
+
+
+const month  =3
+
+switch(month){
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("februaru:");
+        break;
+    case 3:
+        console.log("March");
+        break;
+
+        default: break;
+        console.log("Invalid month");
+
+}
